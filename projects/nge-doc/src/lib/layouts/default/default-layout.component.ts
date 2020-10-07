7baccfcc8bddd9959973cba35a9281a249245ca5
Mamadou Cisse <mciissee@gmail.com>
2020-10-07 16:47:04 +0200

    feat: navigation bar is now closed by default on small devices

--- projects/nge-doc/src/lib/layouts/default/default-layout.component.ts
+++ projects/nge-doc/src/lib/layouts/default/default-layout.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'nge-doc-default-layout',
   templateUrl: './default-layout.component.html',
   styleUrls: ['./default-layout.component.scss']
 })
-export class DefaultLayoutComponent {}
+export class DefaultLayoutComponent implements OnInit, OnDestroy {
+    private subscription?: Subscription;
+    opened = true;
+
+    constructor(
+        private readonly observer: BreakpointObserver
+    ) {}
+
+    ngOnInit() {
+         this.observer.observe([
+            Breakpoints.Small,
+        ]).subscribe(result => {
+            if (result.matches) {
+                this.opened = false;
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.subscription?.unsubscribe();
+    }
+}
